75dc380d678fb2a75410b872aa2cd732ae7f5581
jerem <jeremie.dellea@hotmail.com>
2022-10-11 00:48:06 +0200

    Lien vers la page expedition, creation page expedition

--- app/src/main/java/com/example/applicationprojetsergiojerem/MainActivity.java
+++ app/src/main/java/com/example/applicationprojetsergiojerem/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.applicationprojetsergiojerem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    Button bton_expedition;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bton_expedition = (Button)findViewById(R.id.buttonExpedition);
+
+        //bouton pour passer de la fenêtre de titre à la fenête expédition
+
+        bton_expedition.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ExpeditionActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
