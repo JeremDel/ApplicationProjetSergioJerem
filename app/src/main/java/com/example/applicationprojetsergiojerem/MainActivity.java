c6d28046d21b474c4ba894bafc9ba254b25e0f98
jerem <jeremie.dellea@hotmail.com>
2022-10-11 18:03:25 +0200

    Début mise en place du bouton toolbar

--- app/src/main/java/com/example/applicationprojetsergiojerem/MainActivity.java
+++ app/src/main/java/com/example/applicationprojetsergiojerem/MainActivity.java
@@ -4,9 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,9 +21,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        bton_expedition = (Button)findViewById(R.id.buttonExpedition);
+
 
         //bouton pour passer de la fenêtre de titre à la fenête expédition
+        bton_expedition = (Button)findViewById(R.id.buttonExpedition);
 
         bton_expedition.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,6 +33,24 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    //menu déroulant
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)  {
+        getMenuInflater().inflate(R.menu.menu_principal, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+
+        switch (item.getItemId()) {
+            case R.id.right_icon:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
 }
