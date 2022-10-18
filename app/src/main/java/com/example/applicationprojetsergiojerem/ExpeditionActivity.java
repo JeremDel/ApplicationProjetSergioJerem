615aa08a41996c1073ffb7b0dbf175ffed2b4a98
jerem <jeremie.dellea@hotmail.com>
2022-10-18 23:30:03 +0200

    Addexpedition + icones

--- app/src/main/java/com/example/applicationprojetsergiojerem/ExpeditionActivity.java
+++ app/src/main/java/com/example/applicationprojetsergiojerem/ExpeditionActivity.java
@@ -2,18 +2,33 @@ package com.example.applicationprojetsergiojerem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ExpeditionActivity extends AppCompatActivity {
 
+    private Button button;
+
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-      
-
-
         setContentView(R.layout.activity_expedition);
+
+        button = (Button) findViewById(R.id.imageButtonAdd);
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                addExpedition();
+            }
+        });
+    }
+    public void addExpedition(){
+        Intent intent = new Intent(this, activity_addexpedition.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/applicationprojetsergiojerem/activity_addexpedition.java
@@ -0,0 +1,9 @@
+package com.example.applicationprojetsergiojerem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class activity_addexpedition extends AppCompatActivity {
+
+
+
+}
