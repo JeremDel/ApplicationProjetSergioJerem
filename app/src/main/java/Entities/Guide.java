d922834306fe3802f4946dc78483f2a6877541a9
Sergio <gomez.c.sergio@outlook.com>
2022-10-19 18:07:15 +0200

    Create getters and constructors for both entities. Change attributs to private and create constructors

--- app/src/main/java/Entities/Guide.java
+++ app/src/main/java/Entities/Guide.java
@@ -8,14 +8,64 @@ import java.util.Date;
 @Entity
 public class Guide {
     @PrimaryKey
-    public int id;
-    public int phoneNumber;
+    private int id;
+    private int phoneNumber;
 
-    public Date birthdate;
+    private Date birthdate;
 
-    public String name;
-    public String lastName;
-    public String description;
-    public String address;
-    public String email;
+    private String name;
+    private String lastName;
+    private String description;
+    private String address;
+    private String email;
+    private String picPath;
+
+    public Guide(int phoneNumber, Date birthdate, String name, String lastName, String description,
+                 String address, String email, String picPath){
+        this.phoneNumber = phoneNumber;
+        this.birthdate = birthdate;
+        this.name = name;
+        this.lastName = lastName;
+        this.description = description;
+        this.address = address;
+        this.email = email;
+        this.picPath = picPath;
+    }
+
+    // -- Getters --
+    public int getId() {
+        return id;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public Date getBirthdate() {
+        return birthdate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPicPath() {
+        return picPath;
+    }
 }
