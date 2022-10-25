0546f051e4dcdaacdd327d377e7230fd209c1fc0
jerem <jeremie.dellea@hotmail.com>
2022-10-25 22:36:59 +0200

    menu déroulant enfin presque terminé

--- app/src/main/java/com/example/applicationprojetsergiojerem/MainActivity.java
+++ app/src/main/java/com/example/applicationprojetsergiojerem/MainActivity.java
@@ -1,21 +1,47 @@
 package com.example.applicationprojetsergiojerem;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.Toolbar;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
 
     private Button bton_expedition, btnGuides;
 
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+    if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+        return true;
+    }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,24 +69,55 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-    }
 
-    //menu déroulant
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu)  {
-        getMenuInflater().inflate(R.menu.menu_principal, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.navigationView);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.menu_Open, R.string.close_menu);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item){
+                switch (item.getItemId()) {
+                    case R.id.nav_home:
+                        Log.i("MENU_DRAWER_TAG", "Home item is clicked");
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                        break;
 
-        switch (item.getItemId()) {
-            case R.id.right_icon:
+                    case R.id.nav_search:
+                        Log.i("MENU_DRAWER_TAG", "Search item is clicked");
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                        break;
+
+                    case R.id.nav_users:
+                        Log.i("MENU_DRAWER_TAG", "User item is clicked");
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                        break;
+
+                    case R.id.nav_setting:
+                        Log.i("MENU_DRAWER_TAG", "Settings item is clicked");
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                        break;
+
+                    case R.id.excursionFast:
+                        Log.i("MENU_DRAWER_TAG", "Excursion item is clicked");
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                        break;
+
+                    case R.id.guideFast:
+                        Log.i("MENU_DRAWER_TAG", "Guide item is clicked");
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                        break;
+
+                }
+
                 return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
+            }
+        });
+
+
     }
 
 }
