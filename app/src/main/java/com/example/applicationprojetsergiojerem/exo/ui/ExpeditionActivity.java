d79ae9924b6978d675df2a47d37c13907e0cc01d
Sergio <gomez.c.sergio@outlook.com>
2022-11-01 20:55:53 +0100

    Replace expedition activity code by teacher's

--- app/src/main/java/com/example/applicationprojetsergiojerem/exo/ui/ExpeditionActivity.java
+++ app/src/main/java/com/example/applicationprojetsergiojerem/exo/ui/ExpeditionActivity.java
@@ -1,18 +1,31 @@
 package com.example.applicationprojetsergiojerem.exo.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.applicationprojetsergiojerem.R;
+import com.example.applicationprojetsergiojerem.addexpedition;
+import com.example.applicationprojetsergiojerem.exo.adapter.RecyclerAdapter;
+import com.example.applicationprojetsergiojerem.exo.database.entity.Excursion;
+import com.example.applicationprojetsergiojerem.exo.util.RecyclerViewItemClickListener;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ExpeditionActivity extends AppCompatActivity {
 
+    /*
     private ImageButton button;
     private ImageView imageMuveran, image7lacs, imageWalser;
     private TextView titreMuveran, textMuveran, titre7lacs, text7lacs, titreWalser, textWalser;
@@ -20,9 +33,7 @@ public class ExpeditionActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_expedition);
 
         button = (ImageButton) findViewById(R.id.ExpeditionAdd);
@@ -58,6 +69,65 @@ public class ExpeditionActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+     */
+
+    private List<Excursion> excursions;
+    private RecyclerAdapter<Excursion> adapter;
+    private ExcursionListViewModel viewModel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        getLayoutInflater().inflate(R.layout.activity_expedition, frameLayout); // TODO Check what does the frame layout do
+
+        setTitle(getString(R.string.title_excursion_list));
+
+        RecyclerView recyclerView = findViewById(R.id.excursionRecyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.addItemDecoration(new DividerItemDecoration(recyclerView.getContext(), DividerItemDecoration.HORIZONTAL));
+
+        excursions = new ArrayList<>();
+        adapter = new RecyclerAdapter<>(new RecyclerViewItemClickListener() {
+            @Override
+            public void onItemClick(View v, int position) {
+                // LOG
+                Log.d("List of excursions", "Clicked in " + position);
+                Log.d("List of excursions", "Clicked on " + excursions.get(position).getName());
+
+                Intent intent = new Intent(ExpeditionActivity.this, ExpeditionDetails.class);
+                intent.putExtra("excursionID", excursions.get(position).getId());
+                startActivity(intent);
+            }
+
+            @Override
+            public void onItemLongClick(View v, int position) {
+                // TODO
+            }
+        });
+
+
+        FloatingActionButton fab = findViewById(R.id.floatingActionButton);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ExpeditionActivity.this, EditExcursionActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        ExcursionListViewModel.Factory factory = new ExcursionListViewModel.Factory(getApplication());
+
+        viewModel = ViewModelProviders.of(this, factory).get(ExcursionListViewModel.class);
+        viewModel.getExcursions().observe(this, excursionEntities -> {
+            if (excursionEntities != null) {
+                excursions = excursionEntities;
+                adapter.setData(excursions);
+            }
+        });
+
+        recyclerView.setAdapter(adapter);
     }
 
 }
