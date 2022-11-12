67676e046d953d444aa871475ca09d331b607a50
Sergio <gomez.c.sergio@outlook.com>
2022-11-12 11:11:15 +0100

    Fix missing import

--- app/src/main/java/com/example/applicationprojetsergiojerem/GuideList.java
+++ app/src/main/java/com/example/applicationprojetsergiojerem/GuideList.java
@@ -8,6 +8,8 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.applicationprojetsergiojerem.exo.ui.AddGuide;
+import com.example.applicationprojetsergiojerem.exo.ui.GuideDetails;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class GuideList extends AppCompatActivity {
