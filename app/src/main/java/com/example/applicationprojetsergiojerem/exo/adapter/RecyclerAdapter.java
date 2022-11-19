8e2abe2c6c03f26486ad9be35163209e056a18e1
Sergio <gomez.c.sergio@outlook.com>
2022-11-19 12:13:43 +0100

    Create RecyclerAdapter javadoc

--- app/src/main/java/com/example/applicationprojetsergiojerem/exo/adapter/RecyclerAdapter.java
+++ app/src/main/java/com/example/applicationprojetsergiojerem/exo/adapter/RecyclerAdapter.java
@@ -1,7 +1,6 @@
 package com.example.applicationprojetsergiojerem.exo.adapter;
 
 import android.content.Context;
-import android.graphics.BitmapFactory;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -26,11 +25,22 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHo
     private RecyclerViewItemClickListener listener;
     private Context context;
 
+    /**
+     * Constructeur
+     * @param context
+     * @param listener
+     */
     public RecyclerAdapter(Context context, RecyclerViewItemClickListener listener) {
         this.listener = listener;
         this.context = context;
     }
 
+    /**
+     * Recupère le layout choisi lors de la création de l'adapter
+     * @param parent
+     * @param viewType
+     * @return ViewHolder avec le layout choisi. Le ViewHolder contient tous les composants du layout (TextView, EditText, ...)
+     */
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -48,6 +58,11 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHo
         return viewHolder;
     }
 
+    /**
+     * Gère l'assignation de l'adapter. On va modifier les composants graphiques pour leur donner le texte de l'objet recupéré.
+     * @param holder
+     * @param position
+     */
     @Override
     public void onBindViewHolder(RecyclerAdapter.ViewHolder holder, int position) {
         Guide item = data.get(position);
@@ -59,32 +74,62 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHo
         new ImageLoadTask(((Guide) item).getPicPath(), holder.imageItem).execute();
     }
 
+    /**
+     * Comtpe le nombre d'items dans la RecycleView
+     * @return Nombre d'items dans la RecycleView
+     */
     @Override
     public int getItemCount() {
         return (data == null) ? 0 : data.size();
     }
 
+    /**
+     * Modifie la variable data avec la liste d'excursions recupérée de la database.
+     * Si la variable data n'est pas instanciée on l'écrase, sinon on vérifie que les données sont différentes avant d'écraser les vieilles données
+     * @param data
+     */
     public void setData(final List<Guide> data) {
         if (this.data == null) {
             this.data = data;
             notifyItemRangeInserted(0, this.data.size());
         } else {
             DiffUtil.DiffResult result = DiffUtil.calculateDiff(new DiffUtil.Callback() {
+                /**
+                 * Retourne la taille de la variable data de cette classe
+                 * @return Taille de l'ancienne variable data
+                 */
                 @Override
                 public int getOldListSize() {
                     return RecyclerAdapter.this.data.size();
                 }
 
+                /**
+                 * Retourne la taille de la variable data passée en paramètre
+                 * @return Taille de la variable data passée en paramètre
+                 */
                 @Override
                 public int getNewListSize() {
                     return data.size();
                 }
 
+                /**
+                 * Vérifie un item à la fois que les items dans les variables data sont différents en fonction de leur id. Un changement d'ordre fera que cette fonction retourne false.
+                 * @param oldItemPosition Position à vérifier dans l'ancienne variable data
+                 * @param newItemPosition Position à vérifier dans la nouvelle variable data
+                 * @return true si les items sont identiques (même ID), false si les items sont différents
+                 */
                 @Override
                 public boolean areItemsTheSame(int oldItemPosition, int newItemPosition) {
                         return RecyclerAdapter.this.data.get(oldItemPosition).getId() == data.get(newItemPosition).getId();
                 }
 
+                /**
+                 * Vérifie un item à la fois que le contenu des items dans les variables data sont différents en fonction de leur id. Un changement d'ordre fera que cette fonction retourne false.
+                 * Cette méthode sert à vérifier si les items ont été mis à jour, même si l'ordre reste le même.
+                 * @param oldItemPosition Position à vérifier dans l'ancienne variable data
+                 * @param newItemPosition Position à vérifier dans la nouvelle variable data
+                 * @return true si le contenu des items sont identiques, false si le contenu des items sont différents
+                 */
                 @Override
                 public boolean areContentsTheSame(int oldItemPosition, int newItemPosition) {
                     Guide newGuide = (Guide) data.get(newItemPosition);
@@ -108,6 +153,10 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHo
         private final TextView tvNameDetail, info1, info2;
         private final ImageView imageItem;
 
+        /**
+         * Constructeur
+         * @param viewItem
+         */
         ViewHolder(View viewItem) {
             super(viewItem);
 
